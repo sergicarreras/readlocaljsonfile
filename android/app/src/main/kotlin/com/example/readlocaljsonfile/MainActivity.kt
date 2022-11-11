af72160cbe699a8307cfecfc17277e05bb888c74
sergicarreras <sergi.carreras@salesians.cat>
2022-11-11 17:26:04 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/readlocaljsonfile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.readlocaljsonfile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/items.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class Items {
+  String name_english;
+  String name_telugu;
+  String name_catala;
+
+  Items(this.name_english, this.name_telugu, this.name_catala);
+}
